e4536e9916f91a9f1bac18cd89369e395c2b5c1d
GuiBibeau <g.bibeaulaviolette@gmail.com>
2023-09-26 13:25:27 -0500

    move to src directory

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -26,7 +26,7 @@ export default async function OG() {
         }}
       >
         <img
-          src={new URL("../public/logo.svg", import.meta.url).toString()}
+          src={new URL("../../public/logo.svg", import.meta.url).toString()}
           alt="Precedent Logo"
           tw="w-20 h-20 mb-4 opacity-95"
         />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   future: {
     hoverOnlyWhenSupported: true,
   },
